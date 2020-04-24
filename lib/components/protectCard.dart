090f46bd4c42140dc8e5e001718ee79ddb81a91a
aryanvichare <aryanvichare10@gmail.com>
2020-04-24 10:49:30 -0400

    add components for coronavirus screen

--- lib/components/protectCard.dart
+++ lib/components/protectCard.dart
@@ -2,26 +2,80 @@ import 'package:flutter/material.dart';
 import 'package:protect/screens/protectInfoScreen.dart';
 
 class protectCard extends StatelessWidget {
-   String protectTitle;
-   var protectInformation;
-   Image protectImage;
+  String protectTitle;
+  var protectInformation;
+  String imageFilePath;
+  Widget screen;
 
-
-  protectCard({
-    @required this.protectImage,
-    @required this.protectTitle,
-    this.protectInformation
-  });
+  protectCard(
+      {@required this.imageFilePath,
+      @required this.protectTitle,
+      @required this.screen,
+      this.protectInformation});
 
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
-      onTap: (){
-        Navigator.push(context, MaterialPageRoute(builder: (context) => ProtectInformationScreen(protectImage: protectImage,protectTitle: protectTitle,)));
-      },
-      child: Container(
+        onTap: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (context) => screen,
+            ),
+          );
+        },
+        child: InkWell(
+          child: Padding(
+            padding: EdgeInsets.only(left: 20.0, top: 10.0, bottom: 10.0),
+            child: Container(
+              height: 200.0,
+              width: 200.0,
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(10.0),
+                boxShadow: [
+                  BoxShadow(
+                      blurRadius: 6.0,
+                      color: Colors.grey.withOpacity(0.2),
+                      spreadRadius: 5.0)
+                ],
+              ),
+              child: Column(
+                children: <Widget>[
+                  Container(
+                    child: Image.asset(
+                      imageFilePath,
+                      height: 150.0,
+                      fit: BoxFit.cover,
+                    ),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(10.0),
+                        topRight: Radius.circular(10.0),
+                      ),
+                    ),
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: <Widget>[
+                      Container(
+                        margin: EdgeInsets.only(top: 5.0, left: 10.0),
+                        child: Text(protectTitle),
+                      )
+                    ],
+                  )
+                ],
+              ),
+            ),
+          ),
+        ));
+  }
+}
+
+/*
+Container(
         margin: EdgeInsets.all(10.0),
-        width: MediaQuery.of(context).size.width/2,
+        width: MediaQuery.of(context).size.width/1.5,
         child: Stack(
           alignment: Alignment.topCenter,
           children: <Widget>[
@@ -39,7 +93,6 @@ class protectCard extends StatelessWidget {
               ),
               child: Column(
                 children: <Widget>[
-
                   Row(
                     mainAxisAlignment: MainAxisAlignment.center,
                     children: <Widget>[
@@ -60,23 +113,18 @@ class protectCard extends StatelessWidget {
                       ),
                     ],
                   ),
-
                   ClipRRect(
                     borderRadius: BorderRadius.circular(20.0),
                     child: Image(
-                      height: MediaQuery.of(context).size.height/3.5,
-                      width: MediaQuery.of(context).size.width/1.5,
+                      width: MediaQuery.of(context).size.width,
                       image: protectImage.image,
                       fit: BoxFit.fill,
                     ),
                   ),
-
                 ],
               ),
             )
           ],
         ),
       ),
-    );
-  }
-}
+ */
