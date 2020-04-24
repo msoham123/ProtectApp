6940dd2004eddcc2d215702e42528d17d5bf8ab4
Soham Manoli <msoham123@gmail.ocm>
2020-04-24 10:49:30 -0400

    Fixed Expanded Issue

--- lib/screens/signUpScreen.dart
+++ lib/screens/signUpScreen.dart
@@ -17,118 +17,116 @@ class _SignUpScreenState extends State<SignUpScreen> {
     return Scaffold(
       body: SingleChildScrollView(
         child: Container(
-          child: Container(
-            height: MediaQuery.of(context).size.height,
-            width: MediaQuery.of(context).size.width,
-            child: Expanded(
-              child: ListView(
+          height: MediaQuery.of(context).size.height,
+          width: MediaQuery.of(context).size.width,
+          child: ListView(
 //                mainAxisAlignment: MainAxisAlignment.end,
-                children: <Widget>[
+            children: <Widget>[
 
-                  Container(
-                    child: Stack(
-                      children: <Widget>[
-                        SizedBox(
-                          height: MediaQuery.of(context).size.height/3.85,
-                          width: MediaQuery.of(context).size.width,
-                          child: Image.asset("assets/images/signupImage.png", fit: BoxFit.cover,),
-                        ),
-                        Padding(
-                          padding: const EdgeInsets.all(15.0),
-                          child: IconButton(
-                            icon: Icon(Icons.arrow_back, color: Colors.black,size:32),
-                            onPressed: (){
-                              Navigator.pop(context);
-                            },
-                          ),
-                        ),
-                      ],
+              Container(
+                child: Stack(
+                  children: <Widget>[
+                    SizedBox(
+                      height: MediaQuery.of(context).size.height/3.85,
+                      width: MediaQuery.of(context).size.width,
+                      child: Image.asset("assets/images/signupImage.png", fit: BoxFit.cover,),
                     ),
-                  ),
+                    Padding(
+                      padding: const EdgeInsets.all(15.0),
+                      child: IconButton(
+                        icon: Icon(Icons.arrow_back, color: Colors.black,size:32),
+                        onPressed: (){
+                          Navigator.pop(context);
+                        },
+                      ),
+                    ),
+                  ],
+                ),
+              ),
 
-                  Container(
-                    padding: EdgeInsets.symmetric(horizontal: 40, vertical: 50),
-                    height: MediaQuery.of(context).size.height * 0.70,
-                    width: MediaQuery.of(context).size.width,
-                    decoration: BoxDecoration(
-                        color: Colors.white,
-                        borderRadius: BorderRadius.only(
-                            topLeft: Radius.circular(25),
-                            topRight: Radius.circular(25))),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: <Widget>[
-                        Center(
-                          child: FadeAnimation(0.4,Text("Become a Hero",
-                              style: TextStyle(
-                                  color: Color(0xFFF032f42),
-                                  fontSize: 30,
-                                  fontWeight: FontWeight.bold)),
-                        ),),
-                        Center(
-                          child: FadeAnimation(0.4,Text("Sign up to continue",
-                              style: TextStyle(color: Colors.grey, fontSize: 25)),
-                        ),),
-                        SizedBox(height: 40),
-                        FadeAnimation(0.4,TextField(
-                          onChanged: (value) {
-
-                          },
+              Container(
+                padding: EdgeInsets.symmetric(horizontal: 40, vertical: 50),
+                height: MediaQuery.of(context).size.height * 0.70,
+                width: MediaQuery.of(context).size.width,
+                decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(25),
+                        topRight: Radius.circular(25))),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Center(
+                      child: FadeAnimation(0.4,Text("Become a Hero",
                           style: TextStyle(
-                              color: Color(0xFFF234253),
-                              fontWeight: FontWeight.bold),
-                          obscureText: false,
-                          decoration: InputDecoration(
-                            focusedBorder: OutlineInputBorder(
-                                borderSide:
-                                    BorderSide(color: Colors.grey, width: 1.0)),
-                            enabledBorder: OutlineInputBorder(
-                                borderSide:
-                                    BorderSide(color: Colors.grey, width: 1.0)),
-                            labelText: "Email",
-                            labelStyle: TextStyle(
-                                color: Color(0xFFF234253),
-                                fontWeight: FontWeight.bold),
-                            suffixIcon: Icon(
-                              Icons.email,
-                              size: 27,
-                              color: Color(0xFFF032f41),
-                            ),
-                          ),
-                        ),),
-                        SizedBox(height: 10),
-                        FadeAnimation(0.4,TextField(
-                          onChanged: (value) {
+                              color: Color(0xFFF032f42),
+                              fontSize: 30,
+                              fontWeight: FontWeight.bold)),
+                    ),),
+                    Center(
+                      child: FadeAnimation(0.4,Text("Sign up to continue",
+                          style: TextStyle(color: Colors.grey, fontSize: 25)),
+                    ),),
+                    SizedBox(height: 40),
+                    FadeAnimation(0.4,TextField(
+                      onChanged: (value) {
 
-                          },
-                          style: TextStyle(
-                              color: Color(0xFFF234253),
-                              fontWeight: FontWeight.bold),
-                          obscureText: false,
-                          decoration: InputDecoration(
-                            focusedBorder: OutlineInputBorder(
-                                borderSide:
-                                    BorderSide(color: Colors.grey, width: 1.0)),
-                            enabledBorder: OutlineInputBorder(
-                              borderSide: BorderSide(
-                                color: Colors.grey,
-                                width: 1.0,
-                              ),
-                            ),
-                            labelText: "Password",
-                            labelStyle: TextStyle(
-                              color: Color(0xFFF234253),
-                              fontWeight: FontWeight.bold,
-                            ),
-                            suffixIcon: Icon(
-                              Icons.https,
-                              size: 27,
-                              color: Color(0xFFF032f41),
-                            ),
+                      },
+                      style: TextStyle(
+                          color: Color(0xFFF234253),
+                          fontWeight: FontWeight.bold),
+                      obscureText: false,
+                      decoration: InputDecoration(
+                        focusedBorder: OutlineInputBorder(
+                            borderSide:
+                                BorderSide(color: Colors.grey, width: 1.0)),
+                        enabledBorder: OutlineInputBorder(
+                            borderSide:
+                                BorderSide(color: Colors.grey, width: 1.0)),
+                        labelText: "Email",
+                        labelStyle: TextStyle(
+                            color: Color(0xFFF234253),
+                            fontWeight: FontWeight.bold),
+                        suffixIcon: Icon(
+                          Icons.email,
+                          size: 27,
+                          color: Color(0xFFF032f41),
+                        ),
+                      ),
+                    ),),
+                    SizedBox(height: 10),
+                    FadeAnimation(0.4,TextField(
+                      onChanged: (value) {
+
+                      },
+                      style: TextStyle(
+                          color: Color(0xFFF234253),
+                          fontWeight: FontWeight.bold),
+                      obscureText: false,
+                      decoration: InputDecoration(
+                        focusedBorder: OutlineInputBorder(
+                            borderSide:
+                                BorderSide(color: Colors.grey, width: 1.0)),
+                        enabledBorder: OutlineInputBorder(
+                          borderSide: BorderSide(
+                            color: Colors.grey,
+                            width: 1.0,
                           ),
-                        ),),
-                        SizedBox(height: 40),
-                        Center(
+                        ),
+                        labelText: "Password",
+                        labelStyle: TextStyle(
+                          color: Color(0xFFF234253),
+                          fontWeight: FontWeight.bold,
+                        ),
+                        suffixIcon: Icon(
+                          Icons.https,
+                          size: 27,
+                          color: Color(0xFFF032f41),
+                        ),
+                      ),
+                    ),),
+                    SizedBox(height: 40),
+                    Center(
 //                          child: MaterialButton(
 //                            onPressed: () async {
 ////                              try {
@@ -161,24 +159,22 @@ class _SignUpScreenState extends State<SignUpScreen> {
 //                            padding: EdgeInsets.symmetric(
 //                                horizontal: 30, vertical: 20),
 //                          ),
-                        child: Container(
-                          height: MediaQuery.of(context).size.height/14,
-                          width: MediaQuery.of(context).size.width/2.8,
-                          child: FadeAnimation(0.4,FlatButton(
-                            onPressed: () {},
-                            color: Colors.deepPurpleAccent,
-                            shape: RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(10)),
-                            child: Text("Sign Up", style: TextStyle(color: Colors.white),),
-                          ),),
-                        ),
-                        )
-                      ],
+                    child: Container(
+                      height: MediaQuery.of(context).size.height/14,
+                      width: MediaQuery.of(context).size.width/2.8,
+                      child: FadeAnimation(0.4,FlatButton(
+                        onPressed: () {},
+                        color: Colors.deepPurpleAccent,
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(10)),
+                        child: Text("Sign Up", style: TextStyle(color: Colors.white),),
+                      ),),
                     ),
-                  )
-                ],
-              ),
-            ),
+                    )
+                  ],
+                ),
+              )
+            ],
           ),
         ),
       ),
