4ab3ab7a793b11dfd189ecb58dfceafc954ac2b7
aryanvichare <aryanvichare10@gmail.com>
2020-04-24 10:49:30 -0400

    creating posts works

--- lib/screens/create_post.dart
+++ lib/screens/create_post.dart
@@ -1,9 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:protect/services/firebase_storage_service.dart';
 import 'package:protect/services/image_picker_service.dart';
+import 'package:protect/services/posts_service.dart';
 import 'package:provider/provider.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:image_cropper/image_cropper.dart';
 import 'dart:io';
+import 'package:protect/models/post_model.dart';
+import 'package:protect/services/firestore_service.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:modal_progress_hud/modal_progress_hud.dart';
+
+FirebaseUser loggedInUser;
+final _firestore = Firestore.instance;
+final _auth = FirebaseAuth.instance;
+String loggedInUserName = '';
 
 class CreatePostScreen extends StatefulWidget {
   @override
@@ -12,7 +24,14 @@ class CreatePostScreen extends StatefulWidget {
 
 class _CreatePostScreenState extends State<CreatePostScreen> {
   final key = GlobalKey<FormState>();
+  String hashtags = '', description = '';
+  List<String> hashtagList;
+  bool _inAsyncCall = false;
   File imageFile;
+  String imagePath;
+
+  TextEditingController hashtagController = new TextEditingController();
+  TextEditingController descriptionController = new TextEditingController();
 
   Future<void> _pickImage(BuildContext context) async {
     try {
@@ -29,6 +48,12 @@ class _CreatePostScreenState extends State<CreatePostScreen> {
       print(e);
     }
     Navigator.pop(context);
+  }
+
+  Future<String> getImagePath(FirebaseStorageService service) async {
+    return imageFile != null
+        ? await service.uploadToFirebaseStorage(file: imageFile)
+        : "";
   }
 
   Future<void> _takeImage(BuildContext context) async {
@@ -78,112 +103,199 @@ class _CreatePostScreenState extends State<CreatePostScreen> {
     }
   }
 
+  void getCurrentUser() async {
+    final _firestoreService =
+        Provider.of<FirestoreService>(context, listen: false);
+    try {
+      final user = await _auth.currentUser();
+      if (user != null) {
+        loggedInUser = user;
+        DocumentSnapshot ds =
+            await _firestoreService.getUserDocumentSnapshot(loggedInUser.uid);
+        loggedInUserName = ds.data['full_name'];
+      }
+    } catch (e) {
+      print(e);
+    }
+  }
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    getCurrentUser();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.deepPurple,
-        centerTitle: true,
-        title: Text('Create a Post'),
-        automaticallyImplyLeading: true,
-        elevation: 0.0,
-      ),
-      body: Builder(
-        builder: (context) => Container(
-          child: SingleChildScrollView(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                SizedBox(
-                  height: MediaQuery.of(context).size.height / 20,
-                ),
-                GestureDetector(
-                  onTap: () {
-                    _showPhotoOptions(context);
-                  },
-                  child: _decideImageView(context),
-                ),
-                SizedBox(
-                  height: 5,
-                ),
-                Visibility(
-                  visible: imageFile == null ? false : true,
-                  child: FlatButton(
-                      child: Text(
-                        "Crop Image",
-                        style: TextStyle(color: Colors.white),
-                      ),
-                      color: Color.fromARGB(255, 104, 194, 249),
-                      onPressed: () {
-                        cropImage();
-                      }),
-                ),
-                SizedBox(height: MediaQuery.of(context).size.height / 35),
-                Padding(
-                  padding: EdgeInsets.symmetric(
-                    horizontal: MediaQuery.of(context).size.width / 10,
+    return ModalProgressHUD(
+      inAsyncCall: _inAsyncCall,
+      child: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.deepPurple,
+          centerTitle: true,
+          title: Text('Create a Post'),
+          automaticallyImplyLeading: true,
+          elevation: 0.0,
+        ),
+        body: Builder(
+          builder: (context) => Container(
+            child: SingleChildScrollView(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  SizedBox(
+                    height: MediaQuery.of(context).size.height / 20,
                   ),
-                  child: Form(
-                    key: key,
-                    child: Column(
-                      children: <Widget>[
-                        TextFormField(
-                          keyboardType: TextInputType.text,
-                          decoration: InputDecoration(
-                            prefixIcon: Icon(Icons.format_list_numbered),
-                            labelText: "Hashtags",
-                            hintText: "#coronavirus #stayathome",
-                            border: OutlineInputBorder(),
-                          ),
+                  GestureDetector(
+                    onTap: () {
+                      _showPhotoOptions(context);
+                    },
+                    child: _decideImageView(context),
+                  ),
+                  SizedBox(
+                    height: 5,
+                  ),
+                  Visibility(
+                    visible: imageFile == null ? false : true,
+                    child: FlatButton(
+                        child: Text(
+                          "Crop Image",
+                          style: TextStyle(color: Colors.white),
                         ),
-                        SizedBox(height: 8),
-                        TextFormField(
-                          validator: (val) {
-                            if (val.isEmpty) {
-                              return "Please enter some text";
-                            } else {
-                              return null;
-                            }
-                          },
-                          keyboardType: TextInputType.text,
-                          decoration: InputDecoration(
-                            prefixIcon: Icon(Icons.question_answer),
-                            labelText: "Description",
-                            border: OutlineInputBorder(),
-                          ),
-                          maxLines: 5,
-                        ),
-                        SizedBox(height: 10.0),
-                        Container(
-                          width: MediaQuery.of(context).size.width / 1.2,
-                          child: RaisedButton(
-                            padding: EdgeInsets.symmetric(vertical: 5.0),
-                            child: Text(
-                              "Post",
-                              style: TextStyle(
-                                  color: Colors.white, fontSize: 15.0),
-                            ),
-                            shape: RoundedRectangleBorder(
-                              borderRadius: new BorderRadius.circular(5.0),
-                              side: BorderSide(color: Colors.purpleAccent),
-                            ),
-                            textColor: Colors.white,
-                            color: Colors.purpleAccent,
-                            onPressed: () async {
-                              print('tapped');
-                            },
-                          ),
-                        ),
-                      ],
+                        color: Color.fromARGB(255, 104, 194, 249),
+                        onPressed: () {
+                          cropImage();
+                        }),
+                  ),
+                  SizedBox(height: MediaQuery.of(context).size.height / 35),
+                  Padding(
+                    padding: EdgeInsets.symmetric(
+                      horizontal: MediaQuery.of(context).size.width / 10,
                     ),
-                  ),
-                )
-              ],
+                    child: Form(
+                      key: key,
+                      child: Column(
+                        children: <Widget>[
+                          TextFormField(
+                            validator: (val) {
+                              if (val.isEmpty) {
+                                return "Please enter some text";
+                              } else {
+                                return null;
+                              }
+                            },
+                            controller: hashtagController,
+                            onChanged: (val) {
+                              hashtags = val;
+                              hashtags = hashtags.trim();
+                              hashtagList = hashtags.split(' ');
+                              print(hashtagList);
+                            },
+                            keyboardType: TextInputType.text,
+                            decoration: InputDecoration(
+                              prefixIcon: Icon(Icons.format_list_numbered),
+                              labelText: "Hashtags",
+                              hintText: "#coronavirus #stayathome",
+                              border: OutlineInputBorder(),
+                            ),
+                          ),
+                          SizedBox(height: 8),
+                          TextFormField(
+                            validator: (val) {
+                              if (val.isEmpty) {
+                                return "Please enter some text";
+                              } else {
+                                return null;
+                              }
+                            },
+                            controller: descriptionController,
+                            onChanged: (val) {
+                              description = val;
+                            },
+                            keyboardType: TextInputType.text,
+                            decoration: InputDecoration(
+                              prefixIcon: Icon(Icons.question_answer),
+                              labelText: "Description",
+                              border: OutlineInputBorder(),
+                            ),
+                            maxLines: 5,
+                          ),
+                          SizedBox(height: 10.0),
+                          Container(
+                            width: MediaQuery.of(context).size.width / 1.2,
+                            child: RaisedButton(
+                              padding: EdgeInsets.symmetric(vertical: 5.0),
+                              child: Text(
+                                "Post",
+                                style: TextStyle(
+                                    color: Colors.white, fontSize: 15.0),
+                              ),
+                              shape: RoundedRectangleBorder(
+                                borderRadius: new BorderRadius.circular(5.0),
+                                side: BorderSide(color: Colors.purpleAccent),
+                              ),
+                              textColor: Colors.white,
+                              color: Colors.purpleAccent,
+                              onPressed: () async {
+                                if(imageFile != null && key.currentState.validate()) {
+                                  setState(() {
+                                    _inAsyncCall = true;
+                                  });
+                                  final storageService =
+                                  Provider.of<FirebaseStorageService>(context, listen: false);
+                                  imagePath = await getImagePath(storageService);
+
+                                  PostModel instance = new PostModel(
+                                      sender: loggedInUserName,
+                                      description: description,
+                                      hashtags: hashtagList,
+                                      imageURL: imagePath,
+                                      timestamp: "${DateTime
+                                          .now()
+                                          .year}-${DateTime
+                                          .now()
+                                          .month}-${DateTime
+                                          .now()
+                                          .day}"
+                                  );
+                                  createNewPost(context, instance);
+                                  resetForm(context);
+                                  setState(() {
+                                    _inAsyncCall = false;
+                                  });
+                                } else {
+                                  print('please enter a picture');
+                                }
+                              },
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  )
+                ],
+              ),
             ),
           ),
         ),
       ),
     );
+  }
+
+  void resetForm(BuildContext context) {
+    final snackBar = SnackBar(content: Text("Sucess!"));
+    Scaffold.of(context).showSnackBar(snackBar);
+    hashtagController.clear();
+    descriptionController.clear();
+    setState(() {
+      imageFile = null;
+    });
+  }
+
+  void createNewPost(BuildContext context, PostModel newPost) {
+    final _postsService = Provider.of<PostsService>(context, listen: false);
+    _postsService.addNewPost(newPost);
   }
 
   void _showPhotoOptions(BuildContext context) {
