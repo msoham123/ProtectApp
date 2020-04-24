a1e586cfa0c17d16b0e3d4af18decbebda54aeb7
aryanvichare <aryanvichare10@gmail.com>
2020-04-24 10:49:30 -0400

    define chat rooms

---
+++ lib/ChatRooms.dart
@@ -0,0 +1,15 @@
+class ChatRooms {
+  static const String Global = 'Global';
+  static const String COVID = 'Coronavirus';
+  static const String ClimateChange = 'Climate Change';
+  static const String AmazonRainforest = 'Amazon Rainforest';
+  static const String GreatBarrierReef = 'Great Barrier Reef';
+
+  static const List<String> rooms = <String>[
+    Global,
+    COVID,
+    ClimateChange,
+    AmazonRainforest,
+    GreatBarrierReef
+  ];
+}
