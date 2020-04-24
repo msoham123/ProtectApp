8c7084e51089c307c6ef61e66a19937b53acda10
Soham Manoli <msoham123@gmail.com>
2020-04-24 10:49:30 -0400

    UI overflow bug fix

--- lib/screens/protects/coronavirus.dart
+++ lib/screens/protects/coronavirus.dart
@@ -31,7 +31,7 @@ class _CoronavirusScreenState extends State<CoronavirusScreen> {
             MyHeader(
               image: "./assets/icons/Drcorona.svg",
               textTop: "All you need",
-              textBottom: "is to stay at home.",
+              textBottom: "is to stay at\nhome.",
             ),
             Container(
               margin: EdgeInsets.symmetric(horizontal: 20),
