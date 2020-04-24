6802636a84c0a0b5c80b424a4560a6d05853f0ff
Soham Manoli <msoham123@gmail.com>
2020-04-24 10:49:30 -0400

    UI Changes + Date Added

--- lib/main.dart
+++ lib/main.dart
@@ -40,7 +40,7 @@ class MyApp extends StatelessWidget {
         theme: ThemeData(
           primarySwatch: Colors.purple,
         ),
-        home: AuthWidget(),
+        home: FirstScreen(),
       ),
     );
   }
