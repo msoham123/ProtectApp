ffb5e861fd8b4aa50a8576556785afc4ab73c208
aryanvichare <aryanvichare10@gmail.com>
2020-04-24 10:49:30 -0400

    add authentication lifecycle management

---
+++ lib/auth_widget.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:protect/navScreen.dart';
+import 'package:protect/screens/firstScreen.dart';
+import 'package:protect/screens/loginScreen.dart';
+import 'package:protect/services/firebase_auth_service.dart';
+import 'package:provider/provider.dart';
+
+class AuthWidget extends StatefulWidget {
+  @override
+  _AuthWidgetState createState() => _AuthWidgetState();
+}
+
+class _AuthWidgetState extends State<AuthWidget> {
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final authService =
+        Provider.of<FirebaseAuthService>(context, listen: false);
+    return StreamBuilder<User>(
+      stream: authService.onAuthStateChanged,
+      builder: (context, snapshot) {
+        if (snapshot.data != null) {
+          var user = snapshot.data;
+
+          if (user != null) {
+            return NavScreen();
+          } else {
+            return LoginScreen();
+          }
+        } else {
+          return Scaffold(body: Center(child: CircularProgressIndicator()));
+        }
+      },
+    );
+  }
+}
